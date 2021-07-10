ee24c1bddb8f4b56fef6085d684464293edb63c5
Derriel Collins <derrielcollins96@gmail.com>
2021-07-10 12:05:01 -0500

    fixed all compiling bugs for app.js and BusinessList.js

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business.js';
 
-class BusinessList extends React.Compenent {
+class BusinessList extends React.Component {
 	render() {
 		return(
 		<div className="BusinessList">
